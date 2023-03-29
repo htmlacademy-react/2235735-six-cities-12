2fde6ccc07f5bd201c3621c5306067f8e1fb9514
Lesyk Roman <bacardi_martini@mail.ru>
2023-03-29 13:21:53 +0700

    module4-task1

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,43 +1,16 @@
-import Card from '../../components/card/card';
+import Header from '../../components/header/header';
+import CardList from '../../components/card-list/card-list';
+import { Offer } from '../../types/offers';
+
 
 type MainProps = {
-    cardsCount: number;
+    offers : Offer[];
 }
 
-function Main({ cardsCount }: MainProps): JSX.Element {
-  const cards = new Array(cardsCount);
-  cards.fill(<Card />);
-
+function Main({offers }: MainProps): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active" href="/">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header offers={offers}></Header>
 
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
@@ -98,7 +71,7 @@ function Main({ cardsCount }: MainProps): JSX.Element {
                 </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
-                {cards}
+                <CardList offers = {offers}/>
               </div>
             </section>
             <div className="cities__right-section">
